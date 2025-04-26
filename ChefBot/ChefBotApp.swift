6e1aa43f10116a8423b7356bfa8ae39f0ab4b751
longnguyen39 <longnguyen39ios@gmail.com>
2025-04-26 15:30:33 -0700

    Initial Commit

---
+++ ChefBot/ChefBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefBotApp.swift
+//  ChefBot
+//
+//  Created by Long Nguyen on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ChefBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
