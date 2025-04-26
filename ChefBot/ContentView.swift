211f0bca8b6aac8f966553bfdf7c3fdd328bf09e
longnguyen39 <longnguyen39ios@gmail.com>
2025-04-26 15:36:36 -0700

    Init Commit, on process implementing ChatGPT API.

--- ChefBot/ContentView.swift
+++ ChefBot/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var name: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+                ZStack {
+                    VStack {
+                        VStack {
+                            
+                            Text("Welcome to ChefBot!")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal)
+                                .padding(.vertical)
+                            
+                            Image("logo")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                                .clipShape(RoundedRectangle(cornerRadius: 16))
+                                .padding(.bottom)
+                            
+                            //userInput is store in viewModel.email
+                            TextField("What is your name?", text: $name)
+                                .textInputAutocapitalization(.never)
+                                //.modifier(TxtFieldModifier())
+                                //.focused($keyboardFocused)
+                                .keyboardType(.alphabet)
+                                .disableAutocorrection(true)
+                                
+                            
+                            NavigationLink {
+                                //PurposeScreen(user: $user)
+                            } label: {
+//                                ContBtnView()
+//                                    .opacity(user.username.isEmpty ? 0.5 : 1)
+                            }
+                            .padding(.vertical, 4)
+                            
+                            NavigationLink {
+                                //LoginScreen()
+                            } label: {
+                                HStack(spacing: 3) {
+                                    Text("Already have an account?  - ")
+                                        .fontWeight(.regular)
+                                        .foregroundStyle(.gray)
+                                    Text(" Login")
+                                        .fontWeight(.bold)
+                                        .foregroundStyle(.blue)
+                                }
+                                .font(.footnote)
+                            }
+                        }
+    //                    .ignoresSafeArea()
+                        
+                        Spacer()
+                    }
+                    
+//                    if showLoading {
+//                        LoadingView()
+//                    }
+                }
+            }
+        
     }
 }
 
